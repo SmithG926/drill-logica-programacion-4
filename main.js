06e7bab1fbf47081d58338f64bcdf2e870ad9cb1
Smith Gómez <smithsebas.1747@gmail.com>
2025-04-30 12:29:07 -0400

    Creación código para fibonacci vinculada y óptima para html

---
+++ main.js
@@ -0,0 +1,25 @@
+function resultadoFibonacci(){
+    const input = document.getElementById(`fibonacci`).value;
+    const respuesta = document.getElementById(`resultado`);
+    const mensajeError = document.getElementById(`mensajeError`);
+
+    respuesta.textContent = "";
+    mensajeError.textContent = "";
+
+    const num = parseInt(input);
+
+    if (isNaN(num)) {
+        mensajeError.textContent= "❌ Solo puedes colocar números. Intenta de nuevo.";
+        return;
+    }
+    let fibonacci = 0;
+    let fibonacci2 = 1;
+    let fibonacci3 = "";
+    for (i=1; i<=num; i++){
+        fibonacci3 = fibonacci3 + fibonacci + (i < num ? "," : ".");
+        let siguiente = fibonacci + fibonacci2;
+        fibonacci=fibonacci2;
+        fibonacci2 = siguiente;
+    }
+    resultado.textContent = `Los primeros ${num} números de Fibonacci son: ${fibonacci3}`
+}
